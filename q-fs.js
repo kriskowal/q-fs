f2cbe6236a425c98b82374a0fbebc54f35856995
Kris Kowal <kris.kowal@cixar.com>
2012-06-28 11:43:08 -0700

    Correct path in canonical() errors

--- q-fs.js
+++ q-fs.js
@@ -295,12 +295,12 @@ exports.lastModified = function (path) {
 
 exports.canonical = function (path) {
     var result = Q.defer();
-    FS.realpath(path, function (error, path) {
+    FS.realpath(path, function (error, canonicalPath) {
         if (error) {
             error.message = "Can't get canonical path of " + JSON.stringify(path) + " by way of C realpath: " + error.message;
             result.reject(error);
         } else {
-            result.resolve(path);
+            result.resolve(canonicalPath);
         }
     });
     return result.promise;
